9cd03132e03f176d3a812467aa77c788fc5f0ad5
toshishige.kikuchi <toshishige.kikuchi@gmail.com>
2020-11-23 18:33:09 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Taskが正しく追加されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskが追加されませんでした。'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'Taskが正しく更新されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskが更新されませんでした。'
+      render :new
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'Taskは正しく削除されました。'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
