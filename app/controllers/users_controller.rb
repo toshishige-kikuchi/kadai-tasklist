f79a0c6a74d165076e80f0308ba40db4eb884475
toshishige.kikuchi <toshishige.kikuchi@gmail.com>
2020-12-05 20:23:56 +0000

    login repair

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def index
-    @tasks = Task.all.page(params[:page])
   end
 
   def show
