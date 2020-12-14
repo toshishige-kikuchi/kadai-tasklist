306daa83ba18f013770de5e16add2ef596680a39
toshishige.kikuchi <toshishige.kikuchi@gmail.com>
2020-12-14 02:33:42 +0000

    task_controller edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all.page(params[:page])
